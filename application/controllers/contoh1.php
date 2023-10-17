472aae2790dec96314b4eca43c96938ed587ade2
RubenKurnia20 <RubenKurnia20@gmail.com>
2023-10-17 10:55:40 +0700

    pertemuan 1 contoh 1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo "Nama saya Ruben Kurnia Saputra
+             saya tinggal di daerah Bekasi
+             olahraga yang saya sukai adalah
+             Sepak bola";   
+    }
+}
